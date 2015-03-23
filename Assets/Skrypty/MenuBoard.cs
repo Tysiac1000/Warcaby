c449d88a131b5525a7dfac8c3ca22494b119f009
Krzysztof Matysik <kmatysik1000@gmail.com>
2015-03-23 21:35:23 +0100

    klasa do wyświetlania planszy w menu głównym

---
+++ Assets/Skrypty/MenuBoard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBoard : MonoBehaviour {
+
+	// zmienna określająca szybkość obracania planszy
+	public float rotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+
+		// ruch obrotowy planszy w menu głównym
+		float ruch = rotationSpeed * Time.fixedDeltaTime;
+		transform.RotateAround(Vector3.up, ruch * 100 * Time.deltaTime);
+	}
+
+
+
+}
