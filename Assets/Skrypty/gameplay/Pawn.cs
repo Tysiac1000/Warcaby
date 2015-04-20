937f5c7960ab9233a7a20a7dd801b3230377d85a
Michal Miszewski <m.miszewski@01@gmail.com>
2015-04-20 17:45:04 +0200

    klasy pawn i payer

---
+++ Assets/Skrypty/gameplay/Pawn.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+	/// <summary>
+	/// This is Pawn class 
+	/// </summary>
+public class Pawn : MonoBehaviour {
+
+	public GameObject shadow;
+	public int pawnID;
+	public bool isSelected;
+	/// <summary>
+	/// Zmienna przechowujaca  kolory pionków, oraz materialy z których są zrobione 
+	/// </summary>
+	private Material selectedPawnMat,whitePawn,blackPawn;
+	
+	
+	
+	/// <summary>
+	/// Funkcja inicjuje pionka 
+	/// </summary>
+	void Start () {
+		Vector3 position = new Vector3(this.transform.position.x,this.transform.position.y + 0.01F,this.transform.position.z);
+		selectedPawnMat = Resources.Load("selectedPawn", typeof(Material)) as Material;
+		whitePawn = Resources.Load("whitePawn", typeof(Material)) as Material;
+		blackPawn = Resources.Load("blackPawn", typeof(Material)) as Material;
+		isSelected = false;
+	}
+	
+	/// <summary>
+	/// W funkcji Update wywołana jest funkcja OnPawnClick
+	/// </summary>
+	// Update is called once per frame
+	void Update () {
+		OnPawnClick ();
+	}
+	/// <summary>
+	/// Funkcja prywatna sprawdzająca czy pionek został klikniety 
+	/// </summary>
+	private void OnPawnClick() {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rhPawnHit;
+			bool didHit = Physics.Raycast(toMouse, out rhPawnHit, 500.0f);
+			if(didHit && rhPawnHit.collider.gameObject.tag == "Pawn") {
+				if(rhPawnHit.collider.gameObject.transform.parent.gameObject.GetComponent<Player>().playerID == Camera.main.GetComponent<Gameplay>().whoseTurnID)
+				{
+					if(this.gameObject.transform.parent.gameObject.name == "Player1") {
+						this.gameObject.renderer.material = blackPawn;
+					} else this.gameObject.renderer.material = whitePawn;
+					
+					rhPawnHit.collider.gameObject.renderer.material = selectedPawnMat;
+
+					isSelected = false;
+					rhPawnHit.collider.gameObject.GetComponent<Pawn>().isSelected = true;
+				}
+
+			} else {
+				Debug.Log("Kliknęto w pustą przestrzeń");
+			}
+		}
+	}
+	/// <summary>
+	/// Funkcja publiczna umożliwiająca wykonanie ruchu 
+	/// </summary>
+	/// <param name="fieldId">Id pola na które przesunięty ma być pionek</param>
+	public void move(string fieldId) {
+		// szukamy pozycji na którą postawimy pionek
+		Vector3 dest = GameObject.Find ("Board").GetComponent<Board> ().getFieldCenterCoordinate (fieldId);
+		// przenosimy ten pionek na pozycję dest
+		this.gameObject.transform.localPosition = dest;
+	}
+	/// <summary>
+	/// Funkcja publiczna niszcząca pionek
+	/// </summary>
+	public void destroy() {
+		
+	}
+}
---
+++ Assets/Skrypty/gameplay/Player.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+	/// <summary>
+	/// Funkcja publiczna niszcząca pionek
+	/// </summary>
+public class Player : MonoBehaviour {
+	
+	public int time;
+	public int playerID;
+	public string playerType;
+
+	private string[] startPositions;
+	private GameObject[] Pawns;
+	
+	
+	/// <summary>
+	/// Funkcja inicjująca początkowy układ pionków 
+	/// </summary>
+	void Start () {
+		startPositions = new string[] {"A1", "A3", "A5", "A7", "B2", "B4", "B6", "B8", "C1", "C3", "C5", "C7",
+									   "H2", "H4", "H6", "H8", "G1", "G3", "G5", "G7", "F2", "F4", "F6", "F8"};
+		createPawns ();
+	}
+	
+	/// <summary>
+	/// Funkcja 
+	/// </summary>
+	void Update () {
+	
+	}
+	/// <summary>
+	/// Funkcja publiczna tworząca pionki dla gracza 
+	/// </summary>
+	public void createPawns() {
+		Pawns = new GameObject[12];
+		for (int a = 0; a < 12; a++) {
+			// tworzymy obiekt pionka z prefaba
+			Object pPrefab;
+			if (playerID == 1) pPrefab = Resources.Load("pawn_prefab_black");
+			else pPrefab = Resources.Load("pawn_prefab_white");
+			Pawns[a] = (GameObject)Instantiate(pPrefab); 	
+
+			// dodajemy skrypt dla pionka
+			Pawns[a].AddComponent<Pawn>();
+			// przypisujemy nazwę dla pionka
+			Pawns[a].name = "Pawn" + a;
+			// skalujemy obiekt
+			Pawns[a].transform.localScale = new Vector3((float)0.1406877,(float)0.1406877,(float)0.1406877);
+
+			// ustawiamy pionka na polu
+			GameObject board = GameObject.Find("Board");
+			Vector3 pawnPosCor;
+			if (playerID == 1) pawnPosCor = board.GetComponent<Board>().getFieldCenterCoordinate(startPositions[a]);
+			else pawnPosCor = board.GetComponent<Board>().getFieldCenterCoordinate(startPositions[a+12]);
+			Pawns[a].transform.position = pawnPosCor;
+
+			// dodajemy Box collider
+			Pawns[a].AddComponent<BoxCollider>().size = new Vector3((float)3.4,(float)3.6,(float)1.56);
+			// obracamy pionek o 45 stopni
+			Pawns[a].transform.Rotate(0,0,45);
+
+			// przypisujemy pionki dla playera oraz numery id dla pionków
+			if (playerID == 1) {
+				Pawns[a].transform.parent = GameObject.Find("Player1").gameObject.transform;
+				Pawns[a].GetComponent<Pawn>().pawnID = 100 + a + 1;
+				// zmieniamy stan pola na którym stawiamy pionka: 
+				board.GetComponent<Board>().setFieldState(FieldState.NON_EMPTY, startPositions[a]);
+			}
+			if (playerID == 2) {
+				Pawns[a].transform.parent = GameObject.Find("Player2").gameObject.transform;
+				Pawns[a].GetComponent<Pawn>().pawnID = 200 + a + 1;
+				// zmieniamy stan pola na którym stawiamy pionka: 
+				board.GetComponent<Board>().setFieldState(FieldState.NON_EMPTY, startPositions[a+12]);
+			}
+
+			// dodajemy tag dla pionka
+			Pawns[a].tag = "Pawn";
+
+		}
+	}
+	/// <summary>
+	/// Funkcja umożliwiająca wykonanie ruchu graczowi
+	/// </summary>
+	public void makeMove(){
+
+	}
+	/// <summary>
+	/// Funkcja publiczna blokująca ruchy graczowi ktory oczekuje na swoją turę 
+	/// </summary>
+	public void waitForTurn(){
+	
+	}
+}
