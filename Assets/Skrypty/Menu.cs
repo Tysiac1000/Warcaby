45282648ebba3aca07d537cf58096784c024bf8d
calm442 <calm442@gmail.com>
2015-04-20 21:34:27 +0200

    Zmiany w plikach Options oraz Menu

--- Assets/Skrypty/Menu.cs
+++ Assets/Skrypty/Menu.cs
@@ -1,29 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
 
 /// <summary>
 /// Klasa Menu Gównego
 /// </summary>
-
-public class Menu : MonoBehaviour 
+public class Menu : MonoBehaviour {
 
 	/// <param name="panelInfo"></param>
 	/// <param name="mainCanva"></param>
-
 	private GameObject panelInfo;
 	public GameObject mainCanva;
-
+	public GameObject options;
+	public InputField inputGameName;
+	public ToggleGroup toggleColorGroup;
+	
 	/// <summary>
-	/// Uzyskujemy uchwyt do panel info
+	/// Use this for initialization
+	///  Uzyskujemy uchwyt do panel info
 	/// </summary>
-	void Start () {	
+	void Start () {
+		// szukamy obiektu gry 
 		panelInfo = GameObject.Find("panelInfo");
 	}
 
 	/// <summary>
 	/// Nasłuchiwanie na wciśnięcie klawisza ESC
+	/// Update is called once per frame
 	/// </summary>
-	// Update is called once per frame
 	void Update () {
 
 		// Zamknięcie aplikacji w przypadku wciśniecia przycisku powrotu
@@ -34,35 +39,96 @@ public class Menu : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Sprawdzenie platformy i zamknięcie aplikacji
+	/// </summary>
+	public void ExitApplication(){
+		// Sprawdzamy platformę i zamykamy program
+		if(Application.platform == RuntimePlatform.Android){
+			Application.Quit();
+		}
+	}
 
+	#region DO NOWYCH POZIOMÓW
+	/// <summary>
+	///Przejście do nowej sceny - utworzenie rozgrywki na jednym urządzeniu
+	/// </summary>
+	public void CreateGameOnOneDevice(){
+		// ustawiamy tryb gry
+		GameObject.Find ("Options").GetComponent<Options> ().gMode = GameMode.ONE_DEVICE;
+		// ładujemy nowy poziom
+		Application.LoadLevel("Gameplay");
+	}
 
-	// FUNKCJE WŁASNE ----------------------------------------------------
 	/// <summary>
 	///Przejście do nowej sceny - utworzenie rozgrywki
 	/// </summary>
 	public void CreateGame(){
-		Application.LoadLevel("rozgrywka");
+		options.SendMessage ("setGameplayName", inputGameName.text);
+		//Debug.Log(netConf.getGameplayName().ToString());
+
+		string activeToggle = toggleColorGroup.ActiveToggles ().First ().ToString ();
+		switch (activeToggle) {
+			case "toggleWhite (UnityEngine.UI.Toggle)" : options.SendMessage ("setPawnColor", PawnsColors.WHITE);
+				//Debug.Log("WHITE");
+				break;
+			case "toggleBlack (UnityEngine.UI.Toggle)" : options.SendMessage ("setPawnColor", PawnsColors.BLACK);
+			//Debug.Log("BLACK");
+				break;
+		}
+
+		// ustawiamy tryb gry
+		GameObject.Find ("Options").GetComponent<Options> ().gMode = GameMode.HOST;
+
+		Application.LoadLevel("Gameplay");
 	}
+
 	/// <summary>
 	///Przejście do nowej sceny - dołączenie do rozgrywkui
 	/// </summary>
 	public void JoinGame(){
-		Application.LoadLevel("rozgrywka");
+		// ustawiamy tryb gry
+		GameObject.Find ("Options").GetComponent<Options> ().gMode = GameMode.CLIENT;
+
+		Application.LoadLevel("Gameplay");
 	}
+
 	/// <summary>
 	///Przejście do nowej sceny - samouczka
 	/// </summary>
 	public void StartTutorial(){
-
+		Application.LoadLevel("Tutorial");
 	}
+	#endregion
+
+	#region PANEL MENU
+	public void changeMusicVolume() {
+		Slider musicSlider = GameObject.Find("sliderMusic").GetComponent<Slider>();
+		options.SendMessage ("setMusicVolume", musicSlider.value);
+	}
+	public void changeSoundsVolume() {
+		Slider soundsSlider = GameObject.Find("sliderSounds").GetComponent<Slider>();
+		options.SendMessage ("setSoundVolume", soundsSlider.value);
+	}
+	public void changeGraphicQuality() {
+		Slider graphicSlider = GameObject.Find("sliderGraphics").GetComponent<Slider>();
+		options.SendMessage ("setGraphicsQuality", graphicSlider.value);
+	}
+	public void changeShadows() {
+		Toggle graphicSlider = GameObject.Find("toggleShadows").GetComponent<Toggle>();
+		options.SendMessage ("setShadows", graphicSlider.isOn);
+	}
+	#endregion
 
 	#region PANEL INFO
 	/// <summary>
 	/// Pokazujemy panel z informacjami
 	/// </summary>
 	public void Info(){
+		// pokazujemy panel z informacjami
 		panelInfo.SetActive (true);
 	}
+
 	/// <summary>
 	/// Ukrywamy panel z informacjami
 	/// </summary>
@@ -70,14 +136,5 @@ public class Menu : MonoBehaviour
 		panelInfo.SetActive (false);
 	}
 	#endregion
-
-/// <summary>
-/// Sprawdzenie platformy i zamknięcie aplikacji
-/// </summary>
-	public void ExitApplication(){
-		if(Application.platform == RuntimePlatform.Android){
-			Application.Quit();
-		}
-	}
-
+	
 }
--- Assets/Skrypty/Options.cs
+++ Assets/Skrypty/Options.cs
@@ -1,69 +1,117 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-using UnityEditor;
+//using UnityEditor;
+
+// typ wyliczeniowy określający kolor pionków
+public enum PawnsColors{
+	WHITE = 0,
+	BLACK
+}
+
+// typ wyliczeniowy określający tryb gry: na jednym urządzeniu, host, klient
+public enum GameMode{
+	ONE_DEVICE = 0,
+	HOST,
+	CLIENT
+}
 
 /// <summary>
 // Options - Klasa zażądzająca opcjami gry
 /// </summary>
 public class Options : MonoBehaviour {
 
+	public GameMode gMode;
 	// opcje dźwięku
 	public float musicVolume;
+	private float musicVolumeDefault;
 	public float soundsVolume;
+	private float soundsVolumeDefault;
 	public GameObject Music;
 	public GameObject Sounds;
 	// opcje grafiki
 	public bool shadows;
+	private bool shadowsDefault;
 	public int graphicsQuality;
+	private int graphicsQualityDefault;
 	public int boardStyle;
 	public int pawnStyle;
 
 	// Use this for initialization
 	void Start () {
 		// inicjalizujemy ustawienia dźwięku
-		musicVolume = 1;
-		soundsVolume = 1;
+		musicVolumeDefault = musicVolume = 1;
+		soundsVolumeDefault = soundsVolume = 1;
 		// inicjalizujemy ustawienia grafiki
-		shadows = true;
-		graphicsQuality = 3;
+		shadowsDefault = shadows = true;
+		graphicsQualityDefault = graphicsQuality = 3;
 	}
 
-	/*
-	#region OPCJE DZWIEKU
-	public void ZmianyDzwiekuOK(){
-		if (GameObject.Find("ToggleMuzyka").GetComponent<Toggle>().isOn) {
-			Music.audio.volume = 1;
-			musicLoudness = true;
-		} else {
-			Music.audio.volume = 0;
-			musicLoudness = false;
-		}
-
-		if (GameObject.Find("ToggleEfektyDz").GetComponent<Toggle>().isOn) {
-			Sounds.audio.volume = 1;	
-			soundsVolume = true;
-		} else {
-			Sounds.audio.volume = 0;
-			soundsVolume = false;
-		}
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
 	}
 
-	public void ZmianyDzwiekuAnuluj(){
-		GameObject.Find("ToggleMuzyka").GetComponent<Toggle>().isOn = musicLoudness;	
-		GameObject.Find("ToggleEfektyDz").GetComponent<Toggle>().isOn = soundsVolume;	
+	public void backToDefaultSettings() {
+		musicVolume = musicVolumeDefault;
+		GameObject.Find("sliderMusic").GetComponent<Slider>().value = musicVolumeDefault;
+		soundsVolume = soundsVolumeDefault;
+		GameObject.Find("sliderSounds").GetComponent<Slider>().value = soundsVolumeDefault;
+		shadows = shadowsDefault;
+		GameObject.Find("toggleShadows").GetComponent<Toggle>().isOn = shadowsDefault;
+		graphicsQuality = graphicsQualityDefault;
+		GameObject.Find("sliderGraphics").GetComponent<Slider>().value = graphicsQualityDefault;
+	}
+
+	#region OPCJE SIECIOWE
+	public string gameplayName;
+	public PawnsColors pawnColor;
+	
+	public const int gamePort = 25000;
+	
+	public void setGameplayName(string name) {
+		this.gameplayName = name;
+	}
+	public string getGameplayName() {
+		return this.gameplayName;
+	}
+	
+	public void setPawnColor(PawnsColors color) {
+		this.pawnColor = color;
+	}
+	public PawnsColors getPawnColor() {
+		return this.pawnColor;
+	}
+	#endregion
+
+	#region OPCJE DŹWIEKU
+	public void setMusicVolume(float musicVol) {
+		musicVolume = musicVol;
+	}
+	public float getMusicVolume() {
+		return musicVolume;
+	}
+	public void setSoundVolume(float soundVol) {
+		soundsVolume = soundVol;
+	}
+	public float getSoundVolume() {
+		return soundsVolume;
 	}
 	#endregion
 
 	#region OPCJE GRAFIKI
-	public void ZmianyGrafikiOK(){
-		QualitySettings.SetQualityLevel ((int)GameObject.Find ("SliderTex").GetComponent<Slider> ().value, true);
+	public void setGraphicsQuality(int gQ) {
+		graphicsQuality = gQ;
 	}
-	public void ZmianyGrafikiAnuluj(){
-		GameObject.Find("ToggleCienie").GetComponent<Toggle>().isOn = shadows;
-		GameObject.Find ("SliderTex").GetComponent<Slider> ().value = graphicsQuality;
-		GameObject.Find ("SliderEffCz").GetComponent<Slider> ().value = efekty;
+	public int getGraphicsQuality() {
+		return graphicsQuality;
+	}
+	public void setShadows(bool s) {
+		shadows = s;
+	}
+	public bool getShadows() {
+		return shadows;
 	}
 	#endregion
-	*/
+
+
 }
